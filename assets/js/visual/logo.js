da7a9ce28b543a8b7db1a4871a7122ba894a6b0d
dicdaplon <chafik.lekouara@gmail.com>
2025-08-09 20:30:52 +0200

    add : new logo sentences

--- assets/js/visual/logo.js
+++ assets/js/visual/logo.js
@@ -24,7 +24,27 @@ window.addEventListener('DOMContentLoaded', () => {
     "Le saviez-vous ?\nUn harmoniciste peut\ncaler un blues même\npendant ton discours\nde mariage.",
     "Le saviez-vous ?\nUn violoniste accorde\nson instrument pendant\nles deux moitié\ndu concert.",
     "Le saviez-vous ?\nEn jazz, si tu reconnais\nle morceau, c’est que\nt’es trop carré.",
-    "Le saviez-vous ?\nUn standard de jazz\ncommence au piano et\nfinit quand tout\nle monde est partis."
+    "Le saviez-vous ?\nUn standard de jazz\ncommence au piano et\nfinit quand tout\nle monde est partis.",
+  "Le saviez-vous ?\nLes rockeurs considèrent que\n“laver ses cheveux” est un acte bourgeois.",
+  "Le saviez-vous ?\nTout rockeur a déjà cassé\nune guitare sur scène,\nmême si c’était juste\nune guitare Décathlon.",
+  "Le saviez-vous ?\nUn rockeur ne vieillit pas,\nil jaunit comme une vieille\npochette vinyle.",
+  "Le saviez-vous ?\nUn metalleux peut survivre\nun mois avec trois bières,\nun paquet de Monster Munch\net un T-shirt de Motörhead.",
+  "Le saviez-vous ?\nEn Metal, plus le logo d’un groupe\nest illisible, plus il est “true”.",
+  "Le saviez-vous ?\nLes metalleux hibernent\nde novembre à juin,\npuis migrent tous au Hellfest.",
+  "Le saviez-vous ?\nLes jazzmen peuvent improviser\nun solo sur le bip\nde la caisse enregistreuse.",
+  "Le saviez-vous ?\nQuand un jazzman est à l’heure,\nc’est qu’il est en avance\nde trois mesures.",
+  "Le saviez-vous ?\nLes jazzmen appellent “standard”\nun morceau que personne\nne connaît.",
+  "Le saviez-vous ?\nUn concert de jazz peut durer\n3 heures et contenir\n2 chansons.",
+  "Le saviez-vous ?\nUn bluesman a toujours l’air\nd’avoir raté son bus,\nsa vie et son mariage…\ndans cet ordre.",
+  "Le saviez-vous ?\nLes bluesmen transforment\nn’importe quelle météo\nen “ciel triste”.",
+  "Le saviez-vous ?\nChaque chanson de blues parle\nsoit d’une femme partie,\nsoit d’un chien mort,\nparfois des deux.",
+  "Le saviez-vous ?\nLes musiciens funk peuvent porter\nplus de paillettes qu’un sapin\nde Noël sous acide.",
+  "Le saviez-vous ?\nLe funk ne se joue pas :\nil se transpire.",
+  "Le saviez-vous ?\nLes musiciens funk ne vieillissent pas,\nils s’évaporent en vapeur de sueur\nparfumée au patchouli.",
+  "Le saviez-vous ?\nDans la chanson française,\nla pluie est un personnage principal.",
+  "Le saviez-vous ?\nLes textes de chanson française\nsont si poétiques que personne\nne sait vraiment de quoi ils parlent.",
+  "Le saviez-vous ?\nUn chanteur français ne respire pas :\nil soupire en rimes."
+    
   ];
 
   let lastClickTime = 0; // timestamp du dernier clic
